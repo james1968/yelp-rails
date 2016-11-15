c79ef8eaca62763c53c21907de8f04360c9e393e
hanrattyjen <hanratty.jen@gmail.com>
2016-11-15 16:50:10 +0000

    Add validations for restaurant uniqueness. Add a limit 1..5 for a review

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -7,3 +7,11 @@ describe Restaurant, type: :model do
     expect(restaurant).not_to be_valid
   end
 end
+
+describe Restaurant do
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+end
