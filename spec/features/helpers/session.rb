86d80d70428ed6f96539864ba9140a00641ca37a
James <bakerje@mac.com>
2016-11-18 17:02:24 +0000

    final commit for now

---
+++ spec/features/helpers/session.rb
@@ -0,0 +1,19 @@
+module SessionHelpers
+
+  def sign_in(email: 'alice@example.com', password: '12345678')
+    visit '/users/sign_in'
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    click_button 'Log in'
+  end
+
+  def sign_up(email: 'alice@example.com',
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/users/sign_up'
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
+end
